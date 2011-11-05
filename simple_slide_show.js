1c58127701a5f656ede6cf914183e8207465d002
inc2734 <inc@2inc.org>
2011-11-05 16:43:03 +0900

    リンクつけれるように修正

--- simple_slide_show.js
+++ simple_slide_show.js
@@ -5,38 +5,36 @@
 * 			Numeric duration	アニメーション時間（ms）
 * 			Numeric interval	次のアニメーションまでのインターバル（ms）
 */
-( function( jQuery ) {
+( function( $ ) {
 	$.fn.SimpleSlideShow = function( config ) {
 		var canvas = $(this);
-		canvas.children('img').hide();
+		canvas.find('img').hide();
 		$(window).load( function() {
-			canvas.children('img').wrapAll('<div class="simpleSlideShowWrapper"></div>');
+			canvas.wrapInner('<div class="simpleSlideShowWrapper"></div>');
 			var simpleSlideShow = $('.simpleSlideShowWrapper');
 			var defaults = {
-				height : simpleSlideShow.children('img:first').height(),
-				width  : simpleSlideShow.children('img:first').width(),
+				height : simpleSlideShow.find('img:first').height(),
+				width  : simpleSlideShow.find('img:first').width(),
 				duration : 1000,
 				interval : 3000
 			};
-			var config = $.extend( defaults, config );
-			simpleSlideShow.children('img:first').show();
+			config = $.extend( defaults, config );
+			simpleSlideShow.find('img:first').show();
 			simpleSlideShow.css({
 				'height' : config.height,
 				'width' : config.width
 			});
-			var cnt = simpleSlideShow.children('img').length;
+			var cnt = simpleSlideShow.find('img').length;
 			var i = 0;
 			setInterval( function() {
-				var img = simpleSlideShow.children('img').eq( i );
+				var img = simpleSlideShow.find('img').eq( i );
 				i ++;
-				img.fadeOut(config['duration']);
+				img.fadeOut(config.duration);
 				if ( i >= cnt ) {
 					i = 0;
-					simpleSlideShow.children('img').eq( i ).fadeIn( config.duration );
-				} else {
-					img.next().fadeIn( config.duration );
 				}
-			}, config['interval'] );
+				simpleSlideShow.find('img').eq( i ).fadeIn( config.duration );
+			}, config.interval );
 		});
 	};
 })( jQuery );
