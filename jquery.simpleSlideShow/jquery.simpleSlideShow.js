20e1947c784708b076cb909199bae078faf82e43
Takashi Kitajima <inc@2inc.org>
2013-09-19 13:33:28 +0900

    divのスライド対応、YouTube対応、iframe対応は削除

--- jquery.simpleSlideShow/jquery.simpleSlideShow.js
+++ jquery.simpleSlideShow/jquery.simpleSlideShow.js
@@ -1,7 +1,7 @@
 /**
  * Plugin Name: jquery.SimpleSlideShow
  * Description: シンプルなスライドショーを実装するjQueryプラグイン
- * Version: 0.7.6
+ * Version: 0.7.7
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
  * Created : Sep 30, 2011
@@ -57,13 +57,14 @@
 			var simpleSlideShow = canvas.find( '.simpleSlideShowWrapper' );
 			simpleSlideShow.wrapInner( '<div class="simpleSlideShowInner" />' );
 			var simpleSlideShowInner = canvas.find( '.simpleSlideShowInner' );
-			var cnt = simpleSlideShowInner.find( 'img, iframe' ).length;
-			var images = simpleSlideShowInner.find( 'img, iframe' );
+			var cnt = simpleSlideShowInner.children( 'img, div' ).length;
+			var images = simpleSlideShowInner.children( 'img, div' );
 			var timer = null;
 			var moving = false;
 			var now = 0;
 			var clickCount = 0;
 			var slideDirection;
+			var player = [];
 			images.hide();
 			if ( config.showCaption === true ) {
 				simpleSlideShowInner.after(
@@ -82,9 +83,34 @@
 			simpleSlideShow.addClass( config.type );
 
 			$( window ).load( function() {
+				var _player = [];
+				images.each( function( i, e ) {
+					if ( methods.isYoutube( $( e ) ) ) {
+						$( e )
+							.attr( 'id', 'youtube_' + i )
+							.append( '<div id="youtube_iframe_' + i + '" />' );
+						_player[i] = $( e );
+					}
+				} );
+				if ( _player.length ) {
+					var tag = document.createElement('script');
+					tag.src = "//www.youtube.com/iframe_api";
+					var firstScriptTag = document.getElementsByTagName('script')[0];
+					firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+					window.onYouTubeIframeAPIReady = function() {
+						for ( var i in _player ) {
+							var id = methods.getYoutubeId( _player[i] );
+							player[i] = new YT.Player( 'youtube_iframe_' + i, {
+								height: '390',
+								width: '640',
+								videoId: id
+							} );
+						};
+					}
+				}
 				if ( typeof simpleSlideShowNav !== 'undefined' ) {
 					methods.createSimpleSlideShowNav();
-					canvas.find( simpleSlideShowNav ).find( 'ul li' ).on( 'click', function() {
+					simpleSlideShowNav.find( 'ul li' ).on( 'click', function() {
 						if ( ! $( this ).hasClass( 'cur' ) && moving === false && cnt > 1 ) {
 							var key = $( this ).data( 'key' );
 							if ( key < now && config.type == 'slide' )
@@ -95,7 +121,7 @@
 				}
 				if ( typeof simpleSlideShowPrevNextNav !== 'undefined' ) {
 					methods.createSimpleSlideShowPrevNextNav();
-					canvas.find( simpleSlideShowPrevNextNav ).find( 'ul li' ).on( 'click', function() {
+					simpleSlideShowPrevNextNav.find( 'ul li' ).on( 'click', function() {
 						if ( moving === false && cnt > 1 ) {
 							var key = $( this ).data( 'key' );
 							if ( $( this ).hasClass( 'nav_prev' ) && config.type == 'slide' )
@@ -199,6 +225,9 @@
 					}
 				},
 				processAfterMove: function( key ) {
+					for ( var i in player ) {
+						player[i].stopVideo();
+					}
 					moving = false;
 					now = key;
 					if ( typeof simpleSlideShowPrevNextNav !== 'undefined' ) {
@@ -214,13 +243,20 @@
 				},
 				setCurrentClass: function( key ) {
 					if ( typeof simpleSlideShowNav !== 'undefined' ) {
-						canvas.find( simpleSlideShowNav ).find( 'ul li' )
+						simpleSlideShowNav.find( 'ul li' )
 							.removeClass( 'cur' )
 							.eq( key ).addClass( 'cur' );
 					}
 				},
 				setSimpleSlideShowInnerSize: function() {
 					simpleSlideShowInner.css( {
+						width : ''
+					} );
+					simpleSlideShowInner.css( {
+						height: simpleSlideShowInner.find( 'img:first' ).height(),
+						width : simpleSlideShowInner.find( 'img:first' ).width()
+					} );
+					simpleSlideShowInner.children( 'div' ).css( {
 						height: simpleSlideShowInner.find( 'img:first' ).height(),
 						width : simpleSlideShowInner.find( 'img:first' ).width()
 					} );
@@ -257,26 +293,35 @@
 								}
 							}
 							if ( !src ) {
-								if ( e.nodeName == 'IFRAME' ) {
-									var _id = $( e ).attr( 'src' ).match( '\/\/www.youtube.com\/embed\/([^&#]*)' );
-									if ( _id !== null ) {
-										var id = _id[1];
-										src = 'http://img.youtube.com/vi/' + id + '/2.jpg';
+								// 画像のとき
+								if ( e.nodeName == 'IMG' ) {
+									src = simpleSlideShowInner.find( 'img' ).eq( i ).attr( 'src' );
+								}
+								// 画像以外のとき
+								else {
+									// youtubeのとき
+									if ( methods.isYoutube( $( e ) ) ) {
+										var id = methods.getYoutubeId( $( e ) );
+										if ( id !== null )
+											src = 'http://img.youtube.com/vi/' + id + '/2.jpg';
+									} else {
 									}
-								} else {
-									src = canvas.find( 'img' ).eq( i ).attr( 'src' );
 								}
 							}
-							var navhtml = $( '<img />' ).attr( {
-								src: src,
-								alt: i + 1
-							} );
+							if ( src ) {
+								var navhtml = $( '<img />' ).attr( {
+									src: src,
+									alt: i + 1
+								} );
+							}
 						}
 						if ( typeof navhtml !== 'undefined' ) {
 							var nav = $( '<li />' )
 									.addClass( 'nav_' + ( i + 1 ) )
 									.data( 'key', ( i ) )
 									.append( navhtml );
+							if ( methods.isYoutube( $( e ) ) )
+								nav.addClass( 'nav_youtube' );
 							simpleSlideShowNav.find( 'ul' ).append( nav );
 							canvas.append( simpleSlideShowNav );
 						}
@@ -294,6 +339,17 @@
 						methods.setNavPrevKey();
 						methods.setNavNextKey();
 					}
+				},
+				isYoutube: function( obj ) {
+					if ( obj.hasClass( 'youtube' ) && typeof obj.data( 'uri' ) !== 'undefined' )
+						return true;
+				},
+				getYoutubeId: function( obj ) {
+					if ( !methods.isYoutube( obj ) )
+						return;
+					var id = obj.data( 'uri' ).match( /www\.youtube\.com\/watch\?v=([^&#]*)/ );
+					if ( id !== null )
+						return id[1];
 				}
 			};
 
